5a3d1ccc55f722f799c088592a37fd341fd226db
System Administrator <root@Shirleys-MacBook-Pro-2.local>
2020-03-14 15:47:00 -0700

    [chore] Adding Initial Player Data

---
+++ src/scripts.ts
@@ -0,0 +1,14 @@
+let currentPlayer = "";
+
+function displayPlayers() {
+    document.getElementById("get-players").style.display = "none";
+
+    generatePlayerArray();
+
+    playerArray.forEach(playerInArray => {
+        document.getElementById("player-display-list").innerHTML += "<li>" + playerInArray.name + "</li>";
+    });
+
+    let player = document.getElementById("playerName")["value"];
+    currentPlayer = player;
+}
